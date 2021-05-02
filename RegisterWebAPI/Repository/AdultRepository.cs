f9daedd05bf1adb57a78fae1b397e0bcd03c2aaa
MariAsenova <mariaasenova22@gamil.com>
2021-05-02 20:18:20 +0200

    Adult Repository implementation

---
+++ RegisterWebAPI/Repository/AdultRepository.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Familyregister.Data;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace RegisterWebAPI.Repository
+{
+    public class AdultRepository : IRepository<Adult>
+    {
+        protected readonly DataContext context;
+
+        public AdultRepository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<Adult>> GetAll()
+        {
+            return context.Adults.ToList();
+        }
+
+        public async Task<Adult> GetById(int id)
+        {
+            IQueryable<Adult> adults = context.Adults.Where(a => a.Id == id);
+            Adult firstAsync = await adults.FirstAsync(a => a.Id == id);
+            return firstAsync;
+        }
+
+        public async Task Add(Adult entity)
+        {
+            await context.Adults.AddAsync(entity);
+        }
+
+        public async Task Remove(Adult entity)
+        {
+            context.Adults.Remove(entity);
+        }
+    }
+}
---
+++ RegisterWebAPI/Repository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RegisterWebAPI.Repository
+{
+    public interface IRepository<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(int id);
+        Task Add(T entity);
+        //Task AddRange(IList<T> entities);
+        Task Remove(T entity);
+    }
+}
